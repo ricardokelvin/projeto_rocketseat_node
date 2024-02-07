54b043868cd66fef860df696fff2b1233e2b7137
Ricardo Kelvin <rickelvin97@outlook.com>
2024-02-06 23:19:41 -0300

    feat: Poll creation method

---
+++ src/http/routes/create_poll.ts
@@ -0,0 +1,30 @@
+import { FastifyInstance } from "fastify"
+import { prisma } from "../../lib/prisma"
+import { z } from "zod"
+
+
+export async function creatPoll(app: FastifyInstance) {
+    app.post('/polls', async (request, reply) => {
+        const createPollBody = z.object({
+            tittle: z.string(),
+            options: z.array(z.string()),
+        })
+    
+        const { tittle, options } = createPollBody.parse(request.body)
+    
+        const poll = await prisma.poll.create({
+            data: {
+                tittle,
+                options: {
+                    createMany: {
+                        data: options.map(option => {
+                            return { tittle: option }
+                        })
+                    }
+                }
+            }
+        })
+
+        return reply.status(201).send({ pollId: poll.id })
+    })
+}
