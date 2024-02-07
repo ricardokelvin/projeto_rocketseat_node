aeefd0378ca45dd94890938befb650fcfbf49633
Ricardo Kelvin <rickelvin97@outlook.com>
2024-02-06 23:18:19 -0300

    Upd: App descentralizationfrom server

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,27 +1,10 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
-import { z } from 'zod'
+import { creatPoll } from './routes/create_poll';
 
 //METODOS: GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS
 const app = fastify();
 
-const prisma = new PrismaClient();
-
-app.post('/poll', async (request, reply) => {
-    const createPollBody = z.object({
-        tittle: z.string()
-    });
-
-    const { tittle } = createPollBody.parse(request.body);
-
-    const poll = await prisma.poll.create({
-        data: {
-            tittle,
-        }
-    });
-
-    return reply.status(201).send({ pollId: poll.id });
-});
+app.register(creatPoll)
 
 app.listen({ port: 3333 }).then(() => {
     console.log("HTTP Server running now!");
